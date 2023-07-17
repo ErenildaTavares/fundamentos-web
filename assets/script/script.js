8b03fcb68c1be025eacc02e501b4183cb2acae7e
erenilda <erenyldajsilva@gmail.com>
2023-07-17 14:07:10 -0300

    atualizando

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,7 +11,7 @@ function validaNome(){
 document.getElementById('contatoForm').addEventListener('submit', function () {
     var nome = this.querySelector('input[name=nome]'), nome = nome.value;
     var email = this.querySelector('input[name=email]'), email = email.value;
-    var assunto = 'Olá destinatário, \nMeu nome é ' + nome + ' e meu email é ' + email;
+    var assunto= 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
     this.querySelector('input[name=assunto]').setAttribute('value', assunto);
 });
 let nomeOk = false
@@ -21,13 +21,13 @@ let mapa = document.querySelector("#mapa")
 
 nome.style.width = "100%"
 email.style.width = '100%'
-
-function validaNome() {
+ 
+function validaNome(){
     let txtNome = document.querySelector("#txtNome")
-    if (nome.value.length < 2) {
+    if(nome.value.length <2){
         txtNome.innerHTML = "Nome Inválido!"
         txtNome.style.color = "red"
-    } else {
+    }else{
         txtNome.innerHTML = "Nome Válido!"
         txtNome.style.color = "green"
         nomeOk = true
@@ -35,55 +35,55 @@ function validaNome() {
 
 }
 
-function validaEmail() {
+function validaEmail(){
     let txtEmail = document.querySelector("#txtEmail")
 
-    if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+    if(email.value.indexOf("@") ==-1 || email.value.indexOf(".") ==-1){
         txtEmail.innerHTML = "E-mail inválido"
         txtEmail.style.color = "red"
-    } else {
+    }else{
         txtEmail.innerHTML = "E-mail Válido"
         txtEmail.style.color = "green"
         emailOk = true
     }
 }
 
-function validaAssunto() {
+function validaAssunto(){
     let txtAssunto = document.querySelector("#txtAssunto")
 
-    if (assunto.value.length >= 100) {
+    if(assunto.value.length >=100){
         txtAssunto.innerHTML = "Texto muito grande, digite no máximo 100 caracteres "
         txtAssunto.style.color = "red"
         txtAssunto.style.display = "block"
-    } else {
+    }else{
         txtAssunto.style.display = "none"
         assuntoOk = true
     }
 }
 
 let url = "mailto:ctterenildatavares@gmail.com" +
-    "?subject=" + encodeURIComponent("Contato pelo formulário") +
-    "&body=" + encodeURIComponent("Nome: " + nome.value + "\nE-mail: " + email.value + "\nMensagem: " + assunto.value);
+"?subject=" + encodeURIComponent("Contato pelo formulário") +
+"&body=" + encodeURIComponent("Nome: " + nome.value + "\nE-mail: " + email.value + "\nMensagem: " + assunto.value);
+    
 
-
-function enviar() {
-    if (nomeOk == true && emailOk && assuntoOk == true) {
+function enviar(){
+    if(nomeOk == true && emailOk && assuntoOk == true){
         alert('Clique em "Ok" para prosseguir com o envio do email!')
         // Redirecione o usuário para o cliente de e-mail padrão com a URL de envio do e-mail
-        window.location.href = url;
-    } else {
+window.location.href = url;
+    }else{
         alert('Preencha p formulário corretamente antes de enviar!')
     }
-    console.log(assunto.value)
+     console.log(assunto.value)
 }
 
 
-function mapaZoom() {
-    mapa.style.width = "800px"
-    mapa.style.heigth = "600px"
+function mapaZoom(){
+mapa.style.width = "800px"
+mapa.style.heigth ="600px"
 }
 
-function mapaNormal() {
+function mapaNormal(){
     mapa.style.width = "400px"
     mapa.style.heigth = "250px"
 }
